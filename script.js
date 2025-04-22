ba06e1e29a32061d61568b8a8657bbeb7deb6901
ExtazyyDLC <argubin992@gmail.com>
2025-04-22 13:31:11 +0300

    о
    О

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function register() {
     if (username && password) {
         if (!localStorage.getItem('user_' + username)) {
             localStorage.setItem('user_' + username, password);
+            localStorage.setItem('user_key_' + username, ''); // no key yet
             document.getElementById('message').innerText = 'Registered successfully!';
         } else {
             document.getElementById('message').innerText = 'User already exists.';
@@ -29,12 +30,19 @@ function checkLogin() {
         location.href = 'login.html';
     } else {
         document.getElementById('welcome').innerText = 'Welcome, ' + user + '!';
+        const usedKey = localStorage.getItem('user_key_' + user);
+        if (usedKey) {
+            document.getElementById('status').innerText = 'Key already activated.';
+            document.getElementById('downloadSection').style.display = 'block';
+        }
     }
 }
 
 function activateKey() {
     const key = document.getElementById('keyInput').value.trim();
     const deviceId = getDeviceId();
+    const user = localStorage.getItem('extazyyUser');
+
     fetch('keys.json')
         .then(res => res.json())
         .then(data => {
@@ -42,13 +50,15 @@ function activateKey() {
                 if (!data[key].activated) {
                     data[key].activated = true;
                     data[key].deviceId = deviceId;
+                    data[key].username = user;
                     document.getElementById('status').innerText = 'Key activated for ' + data[key].days + ' days.';
                     document.getElementById('downloadSection').style.display = 'block';
-                } else if (data[key].deviceId === deviceId) {
+                    localStorage.setItem('user_key_' + user, key);
+                } else if (data[key].deviceId === deviceId && data[key].username === user) {
                     document.getElementById('status').innerText = 'Key already activated for ' + data[key].days + ' days.';
                     document.getElementById('downloadSection').style.display = 'block';
                 } else {
-                    document.getElementById('status').innerText = 'Key already used on another device.';
+                    document.getElementById('status').innerText = 'Key already used by another user or device.';
                 }
             } else {
                 document.getElementById('status').innerText = 'Invalid key.';
