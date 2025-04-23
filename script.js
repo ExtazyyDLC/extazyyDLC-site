e2c25f1a7f6c7360a93aeebd24241e5b5339ebaa
ExtazyyDLC <argubin992@gmail.com>
2025-04-23 17:43:01 +0300

    л
    Л

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const loginBtn = document.getElementById('loginBtn');
 const loginForm = document.getElementById('loginForm');
+const downloadSection = document.getElementById('downloadSection');
 
 loginBtn.addEventListener('click', () => {
   loginForm.classList.toggle('hidden');
 });
 
 function login() {
-  const user = document.getElementById('username').value;
-  const pass = document.getElementById('password').value;
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+  const key = document.getElementById('keyInput').value.trim();
 
-  if (user && pass) {
-    localStorage.setItem('loggedInUser', user);
-    alert(`Добро пожаловать, ${user}`);
+  if (username && password && key) {
+    localStorage.setItem('loggedInUser', username);
+    localStorage.setItem('activatedKey', key);
+    alert(`Добро пожаловать, ${username}`);
     loginForm.classList.add('hidden');
-    // здесь можно сделать переход в личный кабинет
+    downloadSection.classList.remove('hidden');
   } else {
-    alert("Введите логин и пароль!");
+    alert("Пожалуйста, заполните все поля!");
   }
 }
